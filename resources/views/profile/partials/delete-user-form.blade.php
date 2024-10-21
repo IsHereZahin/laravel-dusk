0e91d12e9d4196106e02f5eb85e3f5a93e9b3e55
isherezahin <abuzahinmohammadnowsin@gmail.com>
2024-10-21 13:59:37 +0600

    Added Dusk attributes and implemented new browser tests for profile update and profile deletion functionality.

--- resources/views/profile/partials/delete-user-form.blade.php
+++ resources/views/profile/partials/delete-user-form.blade.php
@@ -11,10 +11,10 @@
 
     <x-danger-button
         x-data=""
-        x-on:click.prevent="$dispatch('open-modal', 'confirm-user-deletion')"
+        x-on:click.prevent="$dispatch('open-modal', 'confirm-user-deletion')" dusk="delete-account-button"
     >{{ __('Delete Account') }}</x-danger-button>
 
-    <x-modal name="confirm-user-deletion" :show="$errors->userDeletion->isNotEmpty()" focusable>
+    <x-modal name="confirm-user-deletion" dusk="confirm-user-deletion" :show="$errors->userDeletion->isNotEmpty()" focusable>
         <form method="post" action="{{ route('profile.destroy') }}" class="p-6">
             @csrf
             @method('delete')
@@ -46,7 +46,7 @@
                     {{ __('Cancel') }}
                 </x-secondary-button>
 
-                <x-danger-button class="ms-3">
+                <x-danger-button class="ms-3" dusk="confirm-delete-account">
                     {{ __('Delete Account') }}
                 </x-danger-button>
             </div>
--- tests/Browser/RegistrationTest.php
+++ tests/Browser/RegistrationTest.php
@@ -8,23 +8,52 @@ use Tests\DuskTestCase;
 class RegistrationTest extends DuskTestCase
 {
     /**
-     * A Dusk test example for the registration form.
-     */
+    * Test to verify successful user registration.
+    */
     public function testSuccessfulRegistration(): void
     {
         $this->browse(function (Browser $browser) {
-            $browser->visit('/register');
-
-            $browser->type('name', 'Zahin')
+            $browser->visit('/register')
+                    ->type('name', 'Zahin')
                     ->type('email', 'zahin@example.com')
                     ->type('password', '12345678');
 
             $password = $browser->value('input[name="password"]');
-            $browser->type('password_confirmation', $password);
+            $browser->type('password_confirmation', $password)
+                    ->press('button[type="submit"]')
+                    ->assertPathIs('/dashboard')
+                    ->assertSee('Dashboard');
+        });
+    }
 
-            $browser->press('button[type="submit"]');
-            $browser->assertPathIs('/dashboard');
-            $browser->assertSee('Dashboard');
+    /**
+    * Test to validate updating the user profile.
+    */
+    public function testUpdateProfile(): void
+    {
+        $this->browse(function (Browser $browser) {
+            $browser->visit('/profile')
+                    ->assertSee('Profile Information')
+                    ->type('name', 'Updated Zahin')
+                    ->press('button[type="submit"]')
+                    ->assertSee('Saved.')
+                    ->assertSee('Updated Zahin')
+                    ->assertPathIs('/profile');
+        });
+    }
+
+    /**
+    * Test to ensure successful deletion of a user profile.
+    */
+    public function testDeleteProfile(): void
+    {
+        $this->browse(function (Browser $browser) {
+            $browser->visit('/profile')
+                    ->assertSee('Delete Account')
+                    ->press('@delete-account-button')
+                    ->type('#password', '12345678')
+                    ->press('@confirm-delete-account')
+                    ->assertPathIs('/');
             sleep(3);
         });
     }
